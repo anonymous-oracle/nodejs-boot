d87512c28434fd7bbd66b7b2b5014b69143960af
anonymous-oracle <suhass149@gmail.com>
2021-03-02 11:28:48 +0530

    section 5 chapter 63

--- 01-adding-middleware/app.js
+++ 01-adding-middleware/app.js
@@ -3,25 +3,35 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 // app.use((req, res, next) => {
 //     console.log('In the middleware');
 //     next(); // goes to the next middleware function for execution
 // });
 
-// middlewares always meant to run always should be placed at the top and the next() method should be called inside without returning/sending a response
-app.use('/', (req, res, next) => {
-    console.log('In always running index page');
-    next();
-});
+// // middlewares always meant to run always should be placed at the top and the next() method should be called inside without returning/sending a response
+// app.use('/', (req, res, next) => {
+//     console.log('In always running index page');
+//     next();
+// });
 
 
 app.use('/add-product', (req, res, next) => {
     console.log('In add-product page');
-    res.send('<h1>Add a Product</h1><hr>'); // note that return statement is not used
+    res.send('<form action="/product" method="POST">\
+    <input type="text" name="title" id="">\
+    <button type="submit">Add Product</button></form>'); // note that return statement is not used
+});
+
+app.use('/product', (req, res, next) => {
+    const data = req.body
+    console.log(data);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('In index page');
+    // console.log('In index page');
     res.send('<h1>Hello from Express.js</h1>'); // note that return statement is not used
 });
 
