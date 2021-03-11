7465029d316853fe31187813170b9240917bed98
anonymous-oracle <suhass149@gmail.com>
2021-03-11 23:32:56 +0530

    ...

--- part2/app.js
+++ part2/app.js
@@ -1,2 +1,31 @@
 // import http from 'http'; // not supported
 const http = require('http');
+
+const server = http.createServer();
+
+
+
+server.on('request', (req, res) => {
+    console.log('Request received');
+    // for (const key in req) {
+    //     console.log(key);
+    //     console.log('=====================================================================');
+    // }
+    if (req.url !== '/') {
+        res.write(JSON.stringify({ 'MESSAGE': 'Not sure what you\'re looking for' }));
+        return res.end();
+    }
+    res.write(JSON.stringify({ 'MESSAGE': 'Hello' }));
+    return res.end();
+});
+
+
+
+
+
+
+
+
+
+
+server.listen(3000);
