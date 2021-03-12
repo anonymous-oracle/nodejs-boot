d4f8f5b18dd1d0b5afd6ec30a609dbc8f6770c80
anonymous-oracle <suhass149@gmail.com>
2021-03-12 23:19:52 +0530

    ...

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const imported = require('./tutorial.js');
 console.log(`sum of numbers is: ${imported.sum(1,8,96,6,9,6,3,7)}`);
 console.log(imported);
+console.log(imported.sum(1,5,9,7,5,3,2,4,8,6));
+console.log(imported.PI);
+console.log(imported.someObjectClass);
+console.log(new imported.someObjectClass());
