aaee07f421d009a2129f10085edbd1ae8f3125bb
anonymous-oracle <suhass149@gmail.com>
2021-03-15 23:23:59 +0530

    ...

--- app5.js
+++ app5.js
@@ -1,29 +1,46 @@
 const fs = require('fs');
 
-// create/make directory
+// // create/make directory
 
-fs.mkdir('tutorialDir', (err) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log('Directory created successfully');
-        fs.writeFile('./tutorialDir/example.txt', 'Some data', (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('Data written to file successfully');
-            }
-        });
-    }
-});
+// fs.mkdir('tutorialDir', (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log('Directory created successfully');
+//         fs.writeFile('./tutorialDir/example.txt', 'Some data', (err) => {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log('Data written to file successfully');
+//             }
+//         });
+//     }
+// });
 
-// delete the directory
-fs.rmdir('tutorialDir', (err) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log('Directory deleted successfully');
-    }
-});
+// // delete the directory
+// fs.rmdir('tutorialDir', (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log('Directory deleted successfully');
+//     }
+// });
+
+// // delete non-empty directory
+// fs.unlink('./tutorialDir/example.txt', (err)=>{
+// if(err){
+//    console.log(err) ;
+// } else{
+//     console.log('internal file deleted');
+//     fs.rmdir('tutorialDir', (err)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//         else {
+//             console.log('Directory has been deleted');
+//         }
+//     })
+// }
+// });
